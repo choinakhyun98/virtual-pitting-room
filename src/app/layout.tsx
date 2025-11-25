2ae38dabb9b655e1589e1449dae19f6ff8082582
choinakhyun98 <cnh1624@gmail.com>
2025-11-25 16:57:54 +0900

    google search console용 meta tag 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const roboto = Roboto({
 export const metadata: Metadata = {
   title: "가상 피팅룸",
   description: "간편하게 직접 입어보지 않고 고를 수 있도록 도와드리겠습니다.",
+  verification: {
+    google: "nRYw8EnECkgngEq83dfiuFqakKQM-A8rnhgmxN9CuPw", 
+  },
 };
 
 export default function RootLayout({
