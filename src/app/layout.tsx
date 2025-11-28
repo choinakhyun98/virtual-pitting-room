21623689981b5b45fe6c276fab4e9c88a60daac1
choinakhyun98 <cnh1624@gmail.com>
2025-11-28 16:05:08 +0900

    icon 재설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,14 @@ export const metadata: Metadata = {
   keywords:['fitting room','피팅룸','가상피팅','가상피팅룸','virtual fitting room','virtual fitting'],
   metadataBase: new URL("https://my-fitting-room.vercel.app"),
   openGraph:{
+    title: "가상 피팅룸",
     siteName: '가상 피팅룸',
+    description: "간편하게 직접 입어보지 않고 고를 수 있도록 도와드리겠습니다.",
+    url: "https://my-fitting-room.vercel.app",
     images:{
-      url:"/icon.png"
+      url:"/icon.png",
+      width: 512,
+      height: 512
     },
   },
   icons:{
@@ -32,6 +37,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "가상 피팅룸",
+              url: "https://my-fitting-room.vercel.app",
+              publisher: {
+                "@type": "Organization",
+                name: "가상 피팅룸",
+                logo: {
+                  "@type": "ImageObject",
+                  url: "https://my-fitting-room-vercel.app/icon.png", 
+                  width: 512,
+                  height: 512,
+                },
+              },
+            }),
+          }}
+        />
+      </head>
       <body
         className={`${roboto.className} antialiased`}
       >
